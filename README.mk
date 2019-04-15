926315477eca5f2d9dc5685a3f157a231d2d999c
anhthainguyen <soaptavish1993@gmail.com>
2019-04-15 14:50:27 +0700

    Create README.mk

---
+++ README.mk
@@ -0,0 +1,10 @@
+MVVM (ModelViewViewModel)
+
+Luồng đi:
+View --> ViewModel ---> Model
+View: giao diện
+ViewModel: DataCOntext <---- xử lý, Cần tìm hiểu thêm
+Model: Những thứ như học sinh, sinh viên, xe,...
+
+Project sẽ có hai thư mục: Model, ViewModel <------- Chứa các file liên quan
+Các file liên quan View thì toàn bộ sẽ nằm bên ngoài
