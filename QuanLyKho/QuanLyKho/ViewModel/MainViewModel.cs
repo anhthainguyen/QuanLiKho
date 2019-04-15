d54a9839fbce47b8b01d5383fe8704aad5f30099
anhthainguyen <soaptavish1993@gmail.com>
2019-04-15 15:13:55 +0700

    Add MVVM structure files

---
+++ QuanLyKho/QuanLyKho/ViewModel/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKho.ViewModel
+{
+    public class MainViewModel:BaseViewModel
+    {
+        public MainViewModel()
+        {
+            System.Windows.MessageBox.Show("Thu Nghiem MVVM");
+        }
+    }
+}
