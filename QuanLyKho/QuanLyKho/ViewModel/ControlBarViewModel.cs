2cfcf4417bcc424920e4cf0ca5e90d532496fe33
anhthainguyen <soaptavish1993@gmail.com>
2019-04-22 11:25:28 +0700

    Add window UI

---
+++ QuanLyKho/QuanLyKho/ViewModel/ControlBarViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace QuanLyKho.ViewModel
+{
+    public class ControlBarViewModel : BaseViewModel
+    {
+        #region commands
+        public ICommand CloseWindowCommand { get; set; }
+        #endregion
+
+        public ControlBarViewModel()
+        {
+            CloseWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) => {
+                FrameworkElement window = GetWindowParent(p);
+                var w = (window as Window);
+                if(w!=null)
+                {
+                    w.Close();
+                }
+                });
+        }
+
+        FrameworkElement GetWindowParent(UserControl p)
+        {
+            FrameworkElement parent = p;
+
+            while(parent != null)
+            {
+                parent = parent.Parent as FrameworkElement;
+            }
+
+            return parent;
+        }
+    }
+}
